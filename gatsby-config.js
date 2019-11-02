4262d60442e4a0fe8be670ce68b2c3428dcf83d8
Jason Davis <jason@notthatjason.com>
2019-11-01 22:12:56 -0400

    updates: will admin work after this?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Not That Jason',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A repo demonstrating the use of gatsby and netlify for my personal site and resume and probably other stuff eventually.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
